8654181163b6dd6d8ff954e031e8246da989bdd5
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-07-11 23:59:59 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-6xl text-green-500">Hello AI Saas</p>
+    <p className="text-xl text-green-500">Hello AI SaaS</p>
   );
 }
