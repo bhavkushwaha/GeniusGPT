e92bb59575c2be5ddb1ceb7e06c676e3596e407c
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-08-26 00:46:41 +0530

    API Limit UI Implemented

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,7 @@ import {
   VideoIcon,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "@/components/free-counter";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -65,12 +66,16 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-4">
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src="/logo.png" />
           </div>
@@ -98,6 +103,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
